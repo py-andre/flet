94b270eaa12f8a31e6736a685a54847653b9bbb3
py-andre <andre.fsilva.ter@estacio.br>
2024-05-07 17:32:06 -0300

    teste

--- basico/aula3-image.py
+++ basico/aula3-image.py
@@ -2,11 +2,19 @@ import flet as ft
 
 
 def main(page: ft.Page):
+    page.title = 'Image'
 
     image = ft.Image(
-        src= 'https://wallpapercave.com/wp/wp4850088.jpg',
-        border_radius=ft.border_radius.only(top_left=20, top_right=10),
+        src='https://wallpapercave.com/wp/wp4850088.jpg',
+        border_radius=ft.border_radius.only(top_left=20, top_right=10, bottom_left=10, bottom_right=20),
+        width=300,
+        height=1000,
+        fit=ft.ImageFit.CONTAIN,
+        repeat=ft.ImageRepeat.REPEAT_Y,
+
     )
+
     page.add(image)
+
 
 ft.app(target=main, assets_dir='assets')
