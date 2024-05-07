5326600c0573e642ea7c0d5f7564a720b53c21d1
andreFSilva <andrealekhine@gmail.com>
2024-05-07 16:53:53 -0300

    colocando image

--- basico/aula3-image.py
+++ basico/aula3-image.py
@@ -4,7 +4,7 @@ import flet as ft
 def main(page: ft.Page):
 
     image = ft.Image(
-        src= 'assets/image/python-image.jpg',
+        src= 'https://wallpapercave.com/wp/wp4850088.jpg',
         border_radius=ft.border_radius.only(top_left=20, top_right=10),
     )
     page.add(image)
